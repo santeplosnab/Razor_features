08017716ef186a57b1e198c81b2e435ef24788d9
eshxe <eshxe@mail.ru>
2017-08-24 12:53:16 +0400

    enumerating an array in the view

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -5,20 +5,16 @@ namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public IActionResult Index()
         {
-            Product myProduct = new Product
+            Product[] array =
             {
-                ProductID = 1,
-                Name = "Kayak",
-                Description = "A boat fot one person",
-                Category = "Watersports",
-                Price = 275M
-            };
-
-            ViewBag.StockLevel = 2;
-
-            return View(myProduct);
+                new Product {Name = "Kayak", Price = 275M },
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccrer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+        };
+            return View(array);
         }
     }
 }
